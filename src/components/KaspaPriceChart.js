1af51ef71bfa44983d9abdbdcc57eb5fb2061d7f
When Kas <whenkas@gmail.com>
2024-05-18 17:02:20 -0700

    hiding eth for now, not ready

--- src/components/KaspaPriceChart.js
+++ src/components/KaspaPriceChart.js
@@ -377,7 +377,7 @@ const KaspaPriceChart = () => {
                         style={{ padding: '5px', fontSize: '16px' }}
                     >
                         <option value="btc">BTC</option>
-                        <option value="eth">ETH (Alpha, NOT READY)</option>
+                        {/* <option value="eth">ETH (Alpha, NOT READY)</option> */}
                     </select>
                 </div>
                 {assetSelection === 'eth' && (
